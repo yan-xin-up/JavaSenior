4028ca7974a972871f8c1d41f89e4770ec651240
Ivan Xin <41556180+yan-xin-up@users.noreply.github.com>
2020-12-11 17:15:10 +0800

    senior day03

--- day03/src/buzz/xinyan/java/StringTest.java
+++ day03/src/buzz/xinyan/java/StringTest.java
@@ -26,4 +26,23 @@ public class StringTest {
 
 
     }
+
+    @Test
+
+    public void Test2(){
+        String a1 = "全中国最帅";
+
+        String a2 = new String("东京第一帅逼");
+
+        String replace = a1.replace('帅','美');
+
+        System.out.println(replace);
+        System.out.println(a1);
+
+        String replace1 = a2.replace("东京", "关西");
+
+        System.out.println(a2);
+        System.out.println(replace1);
+
+    }
 }
---
+++ day03/src/buzz/xinyan/java/StringTest1.java
@@ -0,0 +1,12 @@
+package buzz.xinyan.java;
+
+/**
+ * @descriptions: String与其他类型的转换
+ * @author: Ivan
+ * @date: 2020/12/11 11:33
+ * @version: 1.0
+ */
+public class StringTest1 {
+
+
+}
