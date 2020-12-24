1187a8729790ae75a28077919abcbd09009e697a
Ivan Xin <41556180+yan-xin-up@users.noreply.github.com>
2020-12-24 09:38:53 +0800

    senior day22 exer

--- day22/src/buzz/xinyan/exer/Day21exer.java
+++ day22/src/buzz/xinyan/exer/Day21exer.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @program: JavaSenior
@@ -16,22 +17,42 @@ import java.util.Arrays;
  */
 public class Day21exer {
     @Test
-    public void Test21(){
-        String bir = "2017-08-06";
-        DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        TemporalAccessor parse = Formatter.parse(bir);
-        LocalDate localDate = LocalDate.parse(bir, Formatter);
-        Date date = Date.valueOf(localDate);
-        System.out.println(date);
-        String format = Formatter.format(parse);
-        System.out.println(format);
+    public void Test21() {
+//        String bir = "2017-08-06";
+//        DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        TemporalAccessor parse = Formatter.parse(bir);
+//        LocalDate localDate = LocalDate.parse(bir, Formatter);
+//        Date date = Date.valueOf(localDate);
+//        System.out.println(date);
+//        String format = Formatter.format(parse);
+//        System.out.println(format);
 
-        Person [] s =new Person[2];
-        s[0] = new Person("aa",12);
-        s[1] = new Person("ff",12);
-        Arrays.sort(s);
-        int [] a = new int[]{1,2,3};
-        System.out.println(a);
+        Person[] s = new Person[5];
+        s[0] = new Person("aa", 12);
+        s[1] = new Person("ff", 12);
+        s[2] = new Person("ff", 13);
+        s[3] = new Person("ff", 14);
+        s[4] = new Person("ff", 15);
+//        Arrays.sort(s);
+//        int [] a = new int[]{1,2,3};
+//        System.out.println(a);
+//        System.out.println(Arrays.toString(s));
+
+        Comparator com = new Comparator() {
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Person && o2 instanceof Person) {
+                    Person p1 = (Person) o1;
+                    Person p2 = (Person) o2;
+                    return -Double.compare(p1.getAge(), p2.getAge());
+
+                }
+                throw new RuntimeException("hhww");
+            }
+        };
+        Arrays.sort(s, com);
         System.out.println(Arrays.toString(s));
+        
     }
 }
