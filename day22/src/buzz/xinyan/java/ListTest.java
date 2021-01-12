f01a5c9aef4efcc2f56bf71f9b2799f7fe860ec0
Ivan Xin <41556180+yan-xin-up@users.noreply.github.com>
2021-01-12 17:19:37 +0800

    senior day22 I

--- day22/src/buzz/xinyan/java/ListTest.java
+++ day22/src/buzz/xinyan/java/ListTest.java
@@ -1,5 +1,13 @@
 package buzz.xinyan.java;
 
+import buzz.xinyan.exer.Person;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @descriptions: List接口的练习
  * @author: Ivan
@@ -7,4 +15,27 @@ package buzz.xinyan.java;
  * @version: 1.0
  */
 public class ListTest {
+    @Test
+    public void test1(){
+        ArrayList list = new ArrayList();
+        list.add(123);
+        list.add(new Person("bavy",12));
+
+        System.out.println(list);
+
+        list.add(2,"baby");
+
+        System.out.println(list);
+
+        List strings = Arrays.asList(new String[]{"as","ss"});
+        list.addAll(2,strings);
+
+        System.out.println(list);
+        System.out.println(list.size());
+
+        System.out.println(list.get(4));
+
+        System.out.println(list.indexOf("ss"));
+    }
+
 }
