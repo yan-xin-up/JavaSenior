f47b220e13de9cf6faee2d021ef91b3bd557316f
xinyan <xinyan7788@live.com>
2021-05-13 23:51:14 +0800

    review

--- day03/src/buzz/xinyan/exer/abcd12345.java
+++ day03/src/buzz/xinyan/exer/abcd12345.java
@@ -10,22 +10,45 @@ import org.junit.Test;
  */
 public class abcd12345 {
 
-
-
     @Test
-    public void Testabc(){
+    public void Testabc() {
         String s = new String("abcd12345");
         String sb = new String();
         String substring = s.substring(1, 8);
         System.out.println(substring);
 
         char[] chars = substring.toCharArray();
-        for(int i = chars.length-1; i >= 0; i--){
-            sb +=chars[i];
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sb += chars[i];
         }
         System.out.println(sb);
-        System.out.println(s.substring(0, 1)+ sb + s.substring(8, 9));
+        System.out.println(s.substring(0, 1) + sb + s.substring(8, 9));
 
 
+    }
+
+    public static void main(String[] args) {
+        bcder b = new bcder();
+        System.out.println(b.Test2("abcd12345", 1, 7));
+    }
+
+
+}
+
+class bcder {
+
+    public String Test2(String str, int st, int end) {
+        if (str != null && str.length() != 0) {
+            char[] chars = str.toCharArray();
+
+            for (; st < end; st++, end--) {
+                char temp;
+                temp = chars[end];
+                chars[end] = chars[st];
+                chars[st] = temp;
+            }
+            return new String(chars);
+        }
+        return null;
     }
 }
